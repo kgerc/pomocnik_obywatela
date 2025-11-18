d0a4cf704d46d1e82fe33d5454a967a023c9455b
kgerc <krzysztofgerc10@gmail.com>
2025-11-18 18:27:36 +0100

    removed app logic from the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LandingPage from "./LandingPage.jsx";
-import MainApp from "./MainApp.jsx"; // przenieś tam kod swojej aplikacji
 
 export default function App() {
   return (
     <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/app" element={<MainApp />} />
       </Routes>
     </Router>
   );
